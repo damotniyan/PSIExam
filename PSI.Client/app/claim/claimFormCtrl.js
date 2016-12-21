aa90e36f03db2d6aa5e356d6f97e164f22685909
damotniyan <damotniyan@gmail.com>
2016-12-21 09:59:10 +0800

    Add files via upload

---
+++ PSI.Client/app/claim/claimFormCtrl.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    "use strict";
+    angular
+        .module("claimManagement")
+        .controller("claimFormCtrl",
+                     ["$scope", "petResource", "Upload", "$timeout",
+                     claimFormCtrl]);
+
+    function claimFormCtrl($scope, petResource, Upload, $timeout) {
+        var vm = this;
+        vm.pickedPet = '';
+        vm.files = '';
+
+        petResource.query(function (data) {
+            vm.pickedPet = data[0].petName;
+            vm.pets = data;            
+        });        
+
+        $scope.submit = function () {
+            var fileNames = '';
+
+            for (var i = 0; i < vm.files.length; i++) {
+                fileNames += '\n' + vm.files[i].name; 
+            }
+
+            alert('Thank you for submitting\n' + fileNames + '\n\nfor ' + vm.pickedPet);
+        };
+
+    }
+}());
---
+++ PSI.Client/common/petResource.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("common.services")
+        .factory("petResource",
+                ["$resource",
+                 "appSettings",
+                    petResource])
+
+    function petResource($resource, appSettings) {
+        return $resource(appSettings.serverPath + "/api/pets/:id");
+    }
+}());
