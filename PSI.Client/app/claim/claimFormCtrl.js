31cd8212f8ad9422bc5f2c34f7e6a1ba01a3b67d
doxiet <doxiet@gmail.com>
2017-01-05 11:04:04 +0800

    Added upload to server feature

--- PSI.Client/app/claim/claimFormCtrl.js
+++ PSI.Client/app/claim/claimFormCtrl.js
@@ -3,10 +3,10 @@
     angular
         .module("claimManagement")
         .controller("claimFormCtrl",
-                     ["petResource", "Upload", "$timeout",
+                     ["petResource", "Upload", "$timeout", "appSettings", "$q",
                      claimFormCtrl]);
 
-    function claimFormCtrl(petResource, Upload, $timeout) {
+    function claimFormCtrl(petResource, Upload, $timeout, appSettings, $q) {
         var vm = this;
         vm.pickedPet = '';
         vm.files = '';
@@ -14,12 +14,32 @@
         petResource.query(function (data) {
             vm.pickedPet = data[0].petName;
             vm.pets = data;            
-        });        
-
+        });
         vm.submit = function () {
+            var deferred = $q.defer();
             var fileNames = '';
             for (var i = 0; i < vm.files.length; i++) {
-                fileNames += '\n' + vm.files[i].name; 
+                fileNames += '\n' + vm.files[i].name;
+                var file = vm.files[i];
+                if (!file.$error) {
+                    Upload.upload({                        
+                        url: appSettings.serverPath + '/api/files/upload',
+                        method: 'POST',
+                        data: {
+                            petName: vm.pickedPet,
+                            file: file
+                        }
+                    }).progress(function (evt) {
+                        // get upload percentage
+                        console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+                    }).success(function (data, status, headers, config) {
+                        // file is uploaded successfully
+                        deferred.resolve(data);
+                    }).error(function (data, status, headers, config) {
+                        // file failed to upload
+                        deferred.reject();
+                    });
+                }
             }
             alert('Thank you for submitting\n' + fileNames + '\n\nfor ' + vm.pickedPet);
             vm.files = '';
---
+++ PSI.WebAPI/Controllers/FileController.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using PSI.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace PSI.WebAPI.Controllers
+{
+    [EnableCorsAttribute("http://localhost:22205", "*", "*")]
+    public class FilesController : ApiController
+    {
+        [HttpPost] 
+        public async Task<HttpResponseMessage> Upload()
+        {
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                this.Request.CreateResponse(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            var provider = GetMultipartProvider();
+            var result = await Request.Content.ReadAsMultipartAsync(provider);
+
+            var originalFileName = GetDeserializedFileName(result.FileData.First());
+            var uploadedFileInfo = new FileInfo(result.FileData.First().LocalFileName);
+            var returnData = "UpLoad" + originalFileName + "Successful";
+
+            return this.Request.CreateResponse(HttpStatusCode.OK, new { returnData, uploadedFileInfo });
+        }
+
+        private MultipartFormDataStreamProvider GetMultipartProvider()
+        {
+            var uploadFolder = "~/App_Data/Temporary/Uploads";
+            var root = HttpContext.Current.Server.MapPath(uploadFolder);
+            Directory.CreateDirectory(root);
+            return new MultipartFormDataStreamProvider(root);
+        }
+
+        private string GetDeserializedFileName(MultipartFileData fileData)
+        {
+            var fileName = GetFileName(fileData);
+            return JsonConvert.DeserializeObject(fileName).ToString();
+        }
+
+        public string GetFileName(MultipartFileData fileData)
+        {
+            return fileData.Headers.ContentDisposition.FileName;
+        }
+    }
+}
